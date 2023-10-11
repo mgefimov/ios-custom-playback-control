a08ef09f25d8a41c7cf538573b2e83bc2eb3bb50
Maksim Efimov <mg.efimov97@gmail.com>
2023-10-11 23:07:14 +0600

    skip button hook

--- Tweak.h
+++ Tweak.h
@@ -2,10 +2,19 @@
 #import <AVFoundation/AVFoundation.h>
 #import <AVKit/AVKit.h>
 
+@interface AVButton: UIButton
+@end
+
+@interface AVPlaybackControlsView: UIView
+@property(readonly, nonatomic) AVButton *skipBackButton; // @synthesize skipBackButton=_skipBackButton;
+@property(readonly, nonatomic) AVButton *skipForwardButton; // @synthesize skipForwardButton=_skipForwardButton;
+@end
+
 @interface AVPlayerController : UIViewController
 @property(nonatomic, retain) AVPlayer *player;
 @end
 
 @interface AVPlaybackControlsController : NSObject
 @property(assign, nonatomic) AVPlayerController *playerController;
+@property(retain, nonatomic) AVPlaybackControlsView *playbackControlsView;
 @end
--- Tweak.x
+++ Tweak.x
@@ -1,7 +1,16 @@
 #import "Tweak.h"
 
 %hook AVPlaybackControlsController
--(void)skipButtonTouchUpInside:(id)arg1 {
-	[self.playerController.player pause];
+-(void)skipButtonTouchUpInside:(AVButton *)btn {
+    AVPlayer * player = self.playerController.player;
+    CMTime seekTime;
+    if (btn == self.playbackControlsView.skipBackButton) {
+        seekTime = CMTimeMakeWithSeconds(-5, 1);
+    } else {
+        seekTime = CMTimeMakeWithSeconds(5, 1);
+    }
+    CMTime targetTime = CMTimeAdd(player.currentTime, seekTime);
+
+    [player seekToTime:targetTime];
 }
 %end
